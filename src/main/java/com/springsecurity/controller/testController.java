34bb3a7391f869ed106cc33e03ee566ee7c7afd7
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2024-05-03 08:31:05 -0400

    refactor: modify controllers users access

--- src/main/java/com/springsecurity/controller/testController.java
+++ src/main/java/com/springsecurity/controller/testController.java
@@ -1,15 +1,26 @@
 package com.springsecurity.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/test")
+
+// web securuty
+@PreAuthorize("denyAll()")
 public class testController {
 
     @GetMapping("/greeting")
+    @PreAuthorize("permitAll()")
     public String test() {
         return "Hello World";
+    }
+
+    @GetMapping("/data")
+    @PreAuthorize("hasAuthority('write')")
+    public String data() {
+        return "data 1234";
     }
 }
