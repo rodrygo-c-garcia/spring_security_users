448bb7caba77b100a6639d95f172e887d89a5175
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2024-05-24 09:33:32 -0400

    refactor: add the try catch for the method that generates the token

--- src/main/java/com/springsecurity/util/JwtUtil.java
+++ src/main/java/com/springsecurity/util/JwtUtil.java
@@ -2,6 +2,8 @@ package com.springsecurity.util;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -19,28 +21,38 @@ public class JwtUtil {
     private String userGeneratorToken;
 
     public String generateToken(Authentication authentication) {
-//        algoritmo de encriptacion (firma del token)
-        Algorithm algorithm = Algorithm.HMAC256(this.SECRETKEY);
-//        sujeto del token (usuario)
-        String username = (String) authentication.getPrincipal();
-        String authorities = authentication.getAuthorities().stream()
-                .map(grantedAuthority -> grantedAuthority.getAuthority())
-                .collect(Collectors.joining(","));
+        try {
+    //        algoritmo de encriptacion (firma del token)
+            Algorithm algorithm = Algorithm.HMAC256(this.SECRETKEY);
+    //        sujeto del token (usuario)
+            String username = (String) authentication.getPrincipal();
+            String authorities = authentication.getAuthorities().stream()
+                    .map(grantedAuthority -> grantedAuthority.getAuthority())
+                    .collect(Collectors.joining(","));
 
-        String token = JWT.create()
-//                usuario que genero el token (backend)
-                .withIssuer(this.userGeneratorToken)
-//                usuario al que esta dirigido el token (frontend)
-                .withSubject(username)
-//                permisos que tiene el usuario (claims)
-                .withClaim("authorities", authorities)
-//                momento acuala en el que se genero el token
-                .withIssuedAt(new Date())
-                .withExpiresAt(new Date (System.currentTimeMillis() + 60000))
-                .withJWTId(UUID.randomUUID().toString())
-//                apartir de que fecha es valido el token
-                .withNotBefore(new Date(System.currentTimeMillis()))
-                .sign(algorithm);
-        return token;
+            String token = JWT.create()
+    //                usuario que genero el token (backend)
+                    .withIssuer(this.userGeneratorToken)
+    //                usuario al que esta dirigido el token (frontend)
+                    .withSubject(username)
+    //                permisos que tiene el usuario (claims)
+                    .withClaim("authorities", authorities)
+    //                momento acuala en el que se genero el token
+                    .withIssuedAt(new Date())
+                    .withExpiresAt(new Date (System.currentTimeMillis() + 60000))
+                    .withJWTId(UUID.randomUUID().toString())
+    //                apartir de que fecha es valido el token
+                    .withNotBefore(new Date(System.currentTimeMillis()))
+                    .sign(algorithm);
+            return token;
+
+        } catch (JWTCreationException exception){
+            System.err.println("Error creating JWT: " + exception.getMessage());
+            return null;
+        }
+    }
+
+    public DecodedJWT decodeToken(String token) {
+
     }
 }
