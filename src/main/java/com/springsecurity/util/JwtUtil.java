79f77e32607a564f5a4279e813bad9ad2ca28387
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2024-05-24 18:42:17 -0400

    feat: add function getAllClaims

--- src/main/java/com/springsecurity/util/JwtUtil.java
+++ src/main/java/com/springsecurity/util/JwtUtil.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.Map;
 
 @Component
 public class JwtUtil {
@@ -79,5 +80,7 @@ public class JwtUtil {
         return decodedJWT.getClaim(claim);
     }
 
-
+    public Map<String, Claim> getAllClaims(DecodedJWT decodedJWT) {
+        return decodedJWT.getClaims();
+    }
 }
