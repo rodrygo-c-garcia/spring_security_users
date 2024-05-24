4f024a696a7a7d079babfe7a1f107e4e61b8998d
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2024-05-24 19:43:29 -0400

    feat: add injection jwtUtil

--- src/main/java/com/springsecurity/config/filter/JwtTokenValidator.java
+++ src/main/java/com/springsecurity/config/filter/JwtTokenValidator.java
@@ -1,17 +1,30 @@
 package com.springsecurity.config.filter;
 
+import com.springsecurity.util.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 public class JwtTokenValidator extends OncePerRequestFilter {
+    private JwtUtil jwtUtil;
+
+    public JwtTokenValidator(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        String jwtToken = request.getHeader(HttpHeaders.AUTHORIZATION);
 
+        if (token != null ) {
+            jwtToken = jwtToken.substring(7);
+        }
+        filterChain.doFilter(request, response);
     }
 }
