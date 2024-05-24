828ebd36fd73607e1ddc8aa0ec1286cf3edc02f2
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2024-05-24 18:36:29 -0400

    feat: add method getSpecificClaim

--- src/main/java/com/springsecurity/util/JwtUtil.java
+++ src/main/java/com/springsecurity/util/JwtUtil.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -73,5 +74,10 @@ public class JwtUtil {
         return decodedJWT.getSubject().toString();
     }
 
-    
+    public Claim getSpecificClaim(DecodedJWT decodedJWT, String claim) {
+//        obtenemos el claim del token
+        return decodedJWT.getClaim(claim);
+    }
+
+
 }
