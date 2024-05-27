767640eb3e5c57ea0fd9f63183d56295f2858f8a
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2024-05-27 00:47:51 -0400

    feat: add @Validate to AuthCreateRoleUser

--- src/main/java/com/springsecurity/controller/dto/AuthCreateRoleUser.java
+++ src/main/java/com/springsecurity/controller/dto/AuthCreateRoleUser.java
@@ -1,8 +1,10 @@
 package com.springsecurity.controller.dto;
 
 import jakarta.validation.constraints.Size;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
+@Validated
 public record AuthCreateRoleUser(@Size(max = 3, message = "The user cannot have more than 3 roles") List<String> roleListName) {
 }
